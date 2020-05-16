053d40289625a81711da4991f978ab144b55721d
Admin <Admin@HOME-PC>
2020-05-16 20:52:58 +0500

    [*] Обработал исключение при попытке копирования из DataGrid

--- HomeWork_CS_2/MainWindow.xaml.cs
+++ HomeWork_CS_2/MainWindow.xaml.cs
@@ -1,23 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Xml.Serialization;
-using System.Text.Json;
 using System.Diagnostics;
 using Microsoft.Win32;
+
 
 namespace HomeWork_CS_2
 {
@@ -249,5 +239,6 @@ namespace HomeWork_CS_2
             }
         }
         #endregion
+
     }
 }
